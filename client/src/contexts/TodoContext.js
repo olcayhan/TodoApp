371a2f09ada542867743447735c1a4c594995570
Olcay Han Korkut <kolcayhan@gmail.com>
2022-12-09 14:18:13 +0300

    finazlize the todos modification

--- client/src/contexts/TodoContext.js
+++ client/src/contexts/TodoContext.js
@@ -16,7 +16,7 @@ export const TodoProvider = ({ children }) => {
     const [signin, setSignin] = useState(localStorage.getItem("user"));
     const [userIDtoDB, setUserIDtoDB] = useState(localStorage.getItem("userID"));
     const [todos, setTodos] = useState([])
-
+    const [isRender, setRender] = useState(true)
 
 
     // setting user to take data
@@ -25,22 +25,13 @@ export const TodoProvider = ({ children }) => {
             setUserIDtoDB()
         }
 
-        getTodotoDB(userIDtoDB)
-            .then((res) => {
-                setTodos([...res.data.other])
-            })
-            .catch((err) => { console.log(err.message) });
-    }, [signin, userIDtoDB])
+        getTodos()
+
+        setRender(false)
+    }, [isRender])
 
 
-    useEffect(() => {
-        getTodotoDB(userIDtoDB)
-            .then((res) => {
-                setTodos([...res.data.other])
-            })
-            .catch((err) => { console.log(err.message) });
-    }, [todos, userIDtoDB])
-
+    console.log(isRender)
 
     // ======================= LOGIN =================================
     function loginUser(formData) {
@@ -50,7 +41,7 @@ export const TodoProvider = ({ children }) => {
                 localStorage.setItem("userID", res.data.user._id)
                 localStorage.setItem("user", true)
                 setSignin(true)
-
+                setRender(true)
             })
             .catch((err) => { console.log(err.response.data.message); })
     }
@@ -66,33 +57,42 @@ export const TodoProvider = ({ children }) => {
 
 
     // ========================== TODOS ================================
+
+    function getTodos() {
+        getTodotoDB(userIDtoDB)
+            .then((res) => {
+                setTodos([...res.data.other])
+            })
+            .catch((err) => { console.log(err.message) });
+    }
     function addNewTodos(name) {
         addTodotoDB({ name, important: false, complete: false, userID: userIDtoDB })
-            .then((res) => console.log(res.data))
+            .then((res) => setRender(true))
             .catch((err) => { console.log(err) })
     }
-    function addNewImportantTodos(name){
+    function addNewImportantTodos(name) {
         addTodotoDB({ name, important: true, complete: false, userID: userIDtoDB })
-        .then((res) => console.log(res.data))
-        .catch((err) => { console.log(err) })
+            .then((res) => setRender(true))
+            .catch((err) => { console.log(err) })
+
     }
 
 
     function toggleTodo(id) {
         toggleTodotoDB(id)
-            .then((res) => { console.log(res.data) })
+            .then((res) => { setRender(true) })
             .catch((err) => { console.log(err) })
+
     }
 
     function importantTodos(id) {
         importantTodotoDB(id)
-            .then((res) => { console.log(res.data) })
+            .then((res) => { setRender(true) })
             .catch((err) => { console.log(err) })
-
     }
     function deleteTodos() {
         deleteTodotoDB(userIDtoDB)
-            .then((res) => console.log(res))
+            .then((res) => setRender(true))
             .catch((err) => { console.log(err) })
     }
 
